9c67ac10e494999550993d2db338e26235e9ac3d
jack52110306 <154974706+jack52110306@users.noreply.github.com>
2024-01-02 23:15:36 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -14,36 +14,53 @@ from linebot.models import *
 app = Flask(__name__)
 
 # 必須放上自己的Channel Access Token
-line_bot_api = LineBotApi('tl5gL7hsmGcUd6nOHEAdteD0Sr63XiF6vjMT+g9nzxVeneoQASK3T1j9slVUtUIxRSnkeBV+22N7hebj1hS1k7PZBBPX71dWnglgZTHM4cc5sUoUAJ35zTY/HGI+OOhpC1PUMM0cp/VS1g3cSodTiwdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi('6sOa+OnuPnspMTtDeTPTNPppPkdBfBN/KAuxkFA6/j9yipl6XFxqyh7ZYANJj/+oRSnkeBV+22N7hebj1hS1k7PZBBPX71dWnglgZTHM4ccW+OPt3YV/aH8rpTEopFBfCWYTJY3bVGbjEhfA74OzCQdB04t89/1O/w1cDnyilFU=')
 # 必須放上自己的Channel Secret
 handler = WebhookHandler('d63c55b9cfbd93b271d1864de01dd4a3')
 
-line_bot_api.push_message('Uc9435ce72f8d955afdd36a170269f9ed'
-
-# 監聽所有來自 /callback 的 Post Request
-@app.route("/callback", methods=['POST'])
-def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    # handle webhook body
+# 氣象預報函式
+def forecast(address):
+    area_list = {}
+    # 將主要縣市個別的 JSON 代碼列出
+    json_api = {"宜蘭縣":"F-D0047-001","桃園市":"F-D0047-005","新竹縣":"F-D0047-009","苗栗縣":"F-D0047-013",
+            "彰化縣":"F-D0047-017","南投縣":"F-D0047-021","雲林縣":"F-D0047-025","嘉義縣":"F-D0047-029",
+            "屏東縣":"F-D0047-033","臺東縣":"F-D0047-037","花蓮縣":"F-D0047-041","澎湖縣":"F-D0047-045",
+            "基隆市":"F-D0047-049","新竹市":"F-D0047-053","嘉義市":"F-D0047-057","臺北市":"F-D0047-061",
+            "高雄市":"F-D0047-065","新北市":"F-D0047-069","臺中市":"F-D0047-073","臺南市":"F-D0047-077",
+            "連江縣":"F-D0047-081","金門縣":"F-D0047-085"}
+    msg = '找不到天氣預報資訊。'    # 預設回傳訊息
     try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-
-    return 'OK'
-
-#訊息傳遞區塊
-##### 基本上程式編輯都在這個function #####
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token,message)
+        code = 'CWA-A6AD0826-313E-41D5-AE53-3742E1898790'
+        url = f'https://opendata.cwb.gov.tw/fileapi/v1/opendataapi/F-C0032-001?Authorization={code}&downloadType=WEB&format=JSON'
+        f_data = requests.get(url)   # 取得主要縣市預報資料
+        f_data_json = f_data.json()  # json 格式化訊息內容
+        location = f_data_json['cwbopendata']['dataset']['location']  # 取得縣市的預報內容
+        for i in location:
+            city = i['locationName']    # 縣市名稱
+            wx8 = i['weatherElement'][0]['time'][0]['parameter']['parameterName']    # 天氣現象
+            mint8 = i['weatherElement'][1]['time'][0]['parameter']['parameterName']  # 最低溫
+            maxt8 = i['weatherElement'][2]['time'][0]['parameter']['parameterName']  # 最高溫
+            ci8 = i['weatherElement'][2]['time'][0]['parameter']['parameterName']    # 舒適度
+            pop8 = i['weatherElement'][2]['time'][0]['parameter']['parameterName']   # 降雨機率
+            area_list[city] = f'未來 8 小時{wx8}，最高溫 {maxt8} 度，最低溫 {mint8} 度，降雨機率 {pop8} %'  # 組合成回傳的訊息，存在以縣市名稱為 key 的字典檔裡
+        for i in area_list:
+            if i in address:        # 如果使用者的地址包含縣市名稱
+                msg = area_list[i]  # 將 msg 換成對應的預報資訊
+                # 將進一步的預報網址換成對應的預報網址
+                url = f'https://opendata.cwb.gov.tw/api/v1/rest/datastore/{json_api[i]}?Authorization={code}&elementName=WeatherDescription'
+                f_data = requests.get(url)  # 取得主要縣市裡各個區域鄉鎮的氣象預報
+                f_data_json = f_data.json() # json 格式化訊息內容
+                location = f_data_json['records']['locations'][0]['location']    # 取得預報內容
+                break
+        for i in location:
+            city = i['locationName']   # 取得縣市名稱
+            wd = i['weatherElement'][0]['time'][1]['elementValue'][0]['value']  # 綜合描述
+            if city in address:           # 如果使用者的地址包含鄉鎮區域名稱
+                msg = f'未來八小時天氣{wd}' # 將 msg 換成對應的預報資訊
+                break
+        return msg  # 回傳 msg
+    except:
+        return msg  # 如果取資料有發生錯誤，直接回傳 msg
 
 #主程式
 import os
