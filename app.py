ea3928b0d0e4e3681b1d1b6cf10fc3ea12a203a2
jack52110306 <154974706+jack52110306@users.noreply.github.com>
2024-01-03 17:23:28 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ from linebot.exceptions import (
 )
 from linebot.models import *
 
+import requests, json, time, statistics  # import statistics 函式庫
+
 app = Flask(__name__)
 
 # 必須放上自己的Channel Access Token
@@ -20,49 +22,64 @@ handler = WebhookHandler('d63c55b9cfbd93b271d1864de01dd4a3')
 
 line_bot_api.push_message('Uc9435ce72f8d955afdd36a170269f9ed', TextSendMessage(text='你可以開始了'))
 
-# 氣象預報函式
-def forecast(address):
-    area_list = {}
-    # 將主要縣市個別的 JSON 代碼列出
-    json_api = {"宜蘭縣":"F-D0047-001","桃園市":"F-D0047-005","新竹縣":"F-D0047-009","苗栗縣":"F-D0047-013",
-            "彰化縣":"F-D0047-017","南投縣":"F-D0047-021","雲林縣":"F-D0047-025","嘉義縣":"F-D0047-029",
-            "屏東縣":"F-D0047-033","臺東縣":"F-D0047-037","花蓮縣":"F-D0047-041","澎湖縣":"F-D0047-045",
-            "基隆市":"F-D0047-049","新竹市":"F-D0047-053","嘉義市":"F-D0047-057","臺北市":"F-D0047-061",
-            "高雄市":"F-D0047-065","新北市":"F-D0047-069","臺中市":"F-D0047-073","臺南市":"F-D0047-077",
-            "連江縣":"F-D0047-081","金門縣":"F-D0047-085"}
-    msg = '找不到天氣預報資訊。'    # 預設回傳訊息
+# 目前天氣函式
+def current_weather(address):
+    city_list, area_list, area_list2 = {}, {}, {} # 定義好待會要用的變數
+    msg = '找不到氣象資訊。'                         # 預設回傳訊息
+
+    # 定義取得資料的函式
+    def get_data(url):
+        w_data = requests.get(url)   # 爬取目前天氣網址的資料
+        w_data_json = w_data.json()  # json 格式化訊息內容
+        location = w_data_json['cwbopendata']['location']  # 取出對應地點的內容
+        for i in location:
+            name = i['locationName']                       # 測站地點
+            city = i['parameter'][0]['parameterValue']     # 縣市名稱
+            area = i['parameter'][2]['parameterValue']     # 鄉鎮行政區
+            temp = check_data(i['weatherElement'][3]['elementValue']['value'])                       # 氣溫
+            humd = check_data(round(float(i['weatherElement'][4]['elementValue']['value'] )*100 ,1)) # 相對濕度
+            r24 = check_data(i['weatherElement'][6]['elementValue']['value'])                        # 累積雨量
+            if area not in area_list:
+                area_list[area] = {'temp':temp, 'humd':humd, 'r24':r24}  # 以鄉鎮區域為 key，儲存需要的資訊
+            if city not in city_list:
+                city_list[city] = {'temp':[], 'humd':[], 'r24':[]}       # 以主要縣市名稱為 key，準備紀錄裡面所有鄉鎮的數值
+            city_list[city]['temp'].append(temp)   # 記錄主要縣市裡鄉鎮區域的溫度 ( 串列格式 )
+            city_list[city]['humd'].append(humd)   # 記錄主要縣市裡鄉鎮區域的濕度 ( 串列格式 )
+            city_list[city]['r24'].append(r24)     # 記錄主要縣市裡鄉鎮區域的雨量 ( 串列格式 )
+
+    # 定義如果數值小於 0，回傳 False 的函式
+    def check_data(e):
+        return False if float(e)<0 else float(e)
+
+    # 定義產生回傳訊息的函式
+    def msg_content(loc, msg):
+        a = msg
+        for i in loc:
+            if i in address: # 如果地址裡存在 key 的名稱
+                temp = f"氣溫 {loc[i]['temp']} 度，" if loc[i]['temp'] != False else ''
+                humd = f"相對濕度 {loc[i]['humd']}%，" if loc[i]['humd'] != False else ''
+                r24 = f"累積雨量 {loc[i]['r24']}mm" if loc[i]['r24'] != False else ''
+                description = f'{temp}{humd}{r24}'.strip('，')
+                a = f'{description}。' # 取出 key 的內容作為回傳訊息使用
+                break
+        return a
+
     try:
+        # 因為目前天氣有兩組網址，兩組都爬取
         code = 'CWA-A6AD0826-313E-41D5-AE53-3742E1898790'
-        url = f'https://opendata.cwb.gov.tw/fileapi/v1/opendataapi/F-C0032-001?Authorization={code}&downloadType=WEB&format=JSON'
-        f_data = requests.get(url)   # 取得主要縣市預報資料
-        f_data_json = f_data.json()  # json 格式化訊息內容
-        location = f_data_json['cwbopendata']['dataset']['location']  # 取得縣市的預報內容
-        for i in location:
-            city = i['locationName']    # 縣市名稱
-            wx8 = i['weatherElement'][0]['time'][0]['parameter']['parameterName']    # 天氣現象
-            mint8 = i['weatherElement'][1]['time'][0]['parameter']['parameterName']  # 最低溫
-            maxt8 = i['weatherElement'][2]['time'][0]['parameter']['parameterName']  # 最高溫
-            ci8 = i['weatherElement'][2]['time'][0]['parameter']['parameterName']    # 舒適度
-            pop8 = i['weatherElement'][2]['time'][0]['parameter']['parameterName']   # 降雨機率
-            area_list[city] = f'未來 8 小時{wx8}，最高溫 {maxt8} 度，最低溫 {mint8} 度，降雨機率 {pop8} %'  # 組合成回傳的訊息，存在以縣市名稱為 key 的字典檔裡
-        for i in area_list:
-            if i in address:        # 如果使用者的地址包含縣市名稱
-                msg = area_list[i]  # 將 msg 換成對應的預報資訊
-                # 將進一步的預報網址換成對應的預報網址
-                url = f'https://opendata.cwb.gov.tw/api/v1/rest/datastore/{json_api[i]}?Authorization={code}&elementName=WeatherDescription'
-                f_data = requests.get(url)  # 取得主要縣市裡各個區域鄉鎮的氣象預報
-                f_data_json = f_data.json() # json 格式化訊息內容
-                location = f_data_json['records']['locations'][0]['location']    # 取得預報內容
-                break
-        for i in location:
-            city = i['locationName']   # 取得縣市名稱
-            wd = i['weatherElement'][0]['time'][1]['elementValue'][0]['value']  # 綜合描述
-            if city in address:           # 如果使用者的地址包含鄉鎮區域名稱
-                msg = f'未來八小時天氣{wd}' # 將 msg 換成對應的預報資訊
-                break
-        return msg  # 回傳 msg
+        get_data(f'https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization=CWA-A6AD0826-313E-41D5-AE53-3742E1898790&format=JSON')
+
+        for i in city_list:
+            if i not in area_list2: # 將主要縣市裡的數值平均後，以主要縣市名稱為 key，再度儲存一次，如果找不到鄉鎮區域，就使用平均數值
+                area_list2[i] = {'temp':round(statistics.mean(city_list[i]['temp']),1),
+                                'humd':round(statistics.mean(city_list[i]['humd']),1),
+                                'r24':round(statistics.mean(city_list[i]['r24']),1)
+                                }
+        msg = msg_content(area_list2, msg)  # 將訊息改為「大縣市」
+        msg = msg_content(area_list, msg)   # 將訊息改為「鄉鎮區域」
+        return msg    # 回傳 msg
     except:
-        return msg  # 如果取資料有發生錯誤，直接回傳 msg
+        return msg    # 如果取資料有發生錯誤，直接回傳 msg
 
 #主程式
 import os
